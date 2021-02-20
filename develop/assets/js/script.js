8319864e8461fb36d0f8692e16ab032a836e9bd3
Vanessa G <vlopez_jv@hotmail.com>
2021-02-19 18:01:15 -0800

    created variables and functioning prompt/confirm button for char.Length

--- develop/assets/js/script.js
+++ develop/assets/js/script.js
@@ -1,9 +1,16 @@
 // Assignment code here
-var lowercase ="abcdefghijklmnopqrstuvwxyz";
-var uppercase ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var specialChar= '!@#$%^&*?><:~`';
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var specialChar = '!@#$%^&*?><:~`;/{}][]|+-)(';
 var numberReq = "0123456789";
 
+var lowAnswer = false;
+var upAnswer = false;
+var specAnswer = false;
+var numAnswer = false;
+
+var userChoices = "";
+var randomPassword;
 
 
 // Get references to the #generate element
@@ -19,4 +26,31 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
+
+//password length
+function generatePassword() {
+
+var lowAnswer = false;
+var upAnswer = false;
+var specAnswer = false;
+var numAnswer = false;
+var userChoices = "";
+
+//number count
+  var charlength = window.prompt('How long will you like your password? Pick between 8 through 128');{
+      if (charlength >= 8 && charlength < 128) {
+        var specAnswer = window.confirm('Would you like to include special characters? ex: @8+!');
+      }
+      else {
+        window.alert('Please enter a number between 8-128 to continue.')
+      }
+    }
+  };
+
+
+  
+
+// var lowercaseReq = window.confirm('Would you like to include lowercase letters?');
+// var uppercaseReq = window.confirm('Would you like to include UPPERCASE letters?');
+// var numberReq = window.confirm('Would you like to include numbers? ex:123');
